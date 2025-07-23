e503925354a47757336cd98ac727a008c7d31773
freamwork97 <jsy11123@naver.com>
2025-07-23 09:31:09 +0900

    Create ExhibitonSlider

--- Front/src/pages/HomePage.jsx
+++ Front/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import KakaoMap from '../components/KakaoMap';
+import ExhibitionSlider from '../components/ExhibitionSlider';
+
 function HomePage() {
   return (
     <div className="mt-4">
       <h1 className="mb-2 fw-bold">Today's Hot Fairs</h1>
       <KakaoMap />
       <h1 className="mb-2 fw-bold mt-2">How about something like this?</h1>
+      <ExhibitionSlider />
     </div>
   );
 }
