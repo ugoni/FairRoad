7d994dfa826afe5933bde571fe61622a8b3a895f
freamwork97 <jsy11123@naver.com>
2025-07-24 09:19:52 +0900

    Test TopicSelector

--- Front/src/components/TopicSelector.jsx
+++ Front/src/components/TopicSelector.jsx
@@ -1,3 +1,24 @@
-import React from "react";
+import React from 'react';
+import topicList from '../data/topicList.js';
 import '../css/TopicSelector.css';
 
+function TopicSelector() {
+  const topics = Object.keys(topicList);
+
+  return (
+    <div className="topic-selector">
+      {topics.map((topic) => {
+        const [emoji, ...nameParts] = topic.split(' ');
+        const name = nameParts.join(' ');
+        return (
+          <button key={topic} className="topic-button">
+            <span className="topic-emoji">{emoji}</span>
+            <span className="topic-name">{name}</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default TopicSelector;
