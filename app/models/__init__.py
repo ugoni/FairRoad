90e804612ec8b9154d311e84bf6bfcbe88bc07a9
Nerdy <62089304+kkch1012@users.noreply.github.com>
2025-08-25 17:38:59 +0900

    back 정리

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.exhibition import Exhibition
-from app.models.review import Review, Tag
-from app.models.calendar import CalendarEvent
-from app.models import association  # noqa: F401
-from app.models.message import Conversation, ConversationParticipant, Message, MessageRead
